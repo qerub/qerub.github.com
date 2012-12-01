794a753eef9941c6e3ad3aa019da2172aa6a548a
Christoffer Sawicki <christoffer.sawicki@gmail.com>
2012-12-01 02:22:55 +0100

    Recompile

--- defsoftware-utils.d.ts
+++ defsoftware-utils.d.ts
@@ -1,20 +1,22 @@
+/// <reference path="extdefs/jquery.d.ts" />
+/// <reference path="extdefs/underscore.browser.d.ts" />
 module defsoftware {
-    export module Utils {
-        export function partial(f, ...someArguments: any[]): (args: any[]) => any;
-        export function intersperse(collection: any[], separator: any): any[];
+    module Utils {
+        var partial: (f: any, ...someArguments: any[]) => (...moreArguments: any[]) => any;
+        function intersperse(collection: any[], separator: any): any[];
     }
-    export module HTML {
-        export function makeElement(tagName: string, ...args: any[]): Element;
-        export var a: (args: any[]) => Element;
-        export var button: (args: any[]) => Element;
-        export var div: (args: any[]) => Element;
-        export var em: (args: any[]) => Element;
-        export var h1: (args: any[]) => Element;
-        export var h2: (args: any[]) => Element;
-        export var h3: (args: any[]) => Element;
-        export var li: (args: any[]) => Element;
-        export var p: (args: any[]) => Element;
-        export var strong: (args: any[]) => Element;
-        export var ul: (args: any[]) => Element;
+    module HTML {
+        function makeElement(tagName: string, ...args: any[]): Element;
+        var a: (...args: any[]) => Element;
+        var button: (...args: any[]) => Element;
+        var div: (...args: any[]) => Element;
+        var em: (...args: any[]) => Element;
+        var h1: (...args: any[]) => Element;
+        var h2: (...args: any[]) => Element;
+        var h3: (...args: any[]) => Element;
+        var li: (...args: any[]) => Element;
+        var p: (...args: any[]) => Element;
+        var strong: (...args: any[]) => Element;
+        var ul: (...args: any[]) => Element;
     }
 }
--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-var a = defsoftware.HTML.a;
-var div = defsoftware.HTML.div;
-var em = defsoftware.HTML.em;
-var h1 = defsoftware.HTML.h1;
-var h2 = defsoftware.HTML.h2;
-var li = defsoftware.HTML.li;
-var p = defsoftware.HTML.p;
-var strong = defsoftware.HTML.strong;
-var ul = defsoftware.HTML.ul;
+var a = this.defsoftware.HTML.a;
+var div = this.defsoftware.HTML.div;
+var em = this.defsoftware.HTML.em;
+var h1 = this.defsoftware.HTML.h1;
+var h2 = this.defsoftware.HTML.h2;
+var li = this.defsoftware.HTML.li;
+var p = this.defsoftware.HTML.p;
+var strong = this.defsoftware.HTML.strong;
+var ul = this.defsoftware.HTML.ul;
 var makeRepositoryListItem = (function (repo) {
     return li(a({
         href: repo.homepage || repo.html_url
