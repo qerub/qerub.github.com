7adb040d263452d11baa7521e119a30c1c21b08c
Christoffer Sawicki <christoffer.sawicki@gmail.com>
2013-12-09 18:14:19 +0100

    Link to Ohloh instead of using their ugly badge

--- main.ts
+++ main.ts
@@ -26,21 +26,16 @@ function GitHubAPI(path: string, callback) {
 }
 
 function main() {
-  var ohloh = $("<a href='https://www.ohloh.net/accounts/484?ref=Detailed' target='_top'><img alt='Ohloh profile for Christoffer Sawicki' border='0' height='35' src='https://www.ohloh.net/accounts/484/widgets/account_detailed.gif' width='191' /></a>");
-  
-  ohloh.find("img").css("vertical-align", "middle");
-  
   var repoContainer, gistContainer;
   
   var body = div(
     h1("qerub.github.com"),
     h2(link("My Personal Website", "http://vemod.net/")),
+    h2(link("My Page on Ohloh", "https://www.ohloh.net/accounts/Qerub?ref=Detailed")),
     h2(link("My GitHub Repositories", "https://github.com/qerub")),
     (repoContainer = p(em().html("Loading&hellip;"))),
     h2(link("My Gists", "https://gist.github.com/qerub")),
-    (gistContainer = p(em().html("Loading&hellip;"))),
-    h2("Misc. Open Source Contributions"),
-    p("See Ohloh: ", ohloh)
+    (gistContainer = p(em().html("Loading&hellip;")))
   );
   
   $(document.body).append(body);
