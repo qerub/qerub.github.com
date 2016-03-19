103b1732fd1894c37cf8b5bd4531572366fb84f2
Christoffer Sawicki <christoffer.sawicki@gmail.com>
2016-03-19 13:14:04 +0100

    Be consistent in how string literals are written

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,27 @@
-var gulp = require('gulp');
+var gulp = require("gulp");
 
-gulp.task('compile-typescript', function () {
-  var sourcemaps = require('gulp-sourcemaps');
-  var ts = require('gulp-typescript');
-  var babel = require('gulp-babel');
-  return gulp.src(['src/*.ts', 'src/*.tsx'])
+gulp.task("compile-typescript", function () {
+  var sourcemaps = require("gulp-sourcemaps");
+  var ts = require("gulp-typescript");
+  var babel = require("gulp-babel");
+  return gulp.src(["src/*.ts", "src/*.tsx"])
              .pipe(sourcemaps.init())
-             .pipe(ts({typescript: require('typescript'), jsx: "preserve"}))
+             .pipe(ts({typescript: require("typescript"), jsx: "preserve"}))
              .js
              .pipe(babel())
-             .pipe(sourcemaps.write('.'))
-             .pipe(gulp.dest('dist'));
+             .pipe(sourcemaps.write("."))
+             .pipe(gulp.dest("dist"));
 });
 
-gulp.task('copy-static-files', function () {
-  return gulp.src(['src/index.html', 'src/main.css']).pipe(gulp.dest('dist'));
+gulp.task("copy-static-files", function () {
+  return gulp.src(["src/index.html", "src/main.css"]).pipe(gulp.dest("dist"));
 });
 
-gulp.task('build', ['compile-typescript', 'copy-static-files']);
+gulp.task("build", ["compile-typescript", "copy-static-files"]);
 
-gulp.task('deploy', ['build'], function () {
-  var gitHubPages = require('gulp-gh-pages')({branch: 'master'});
-  return gulp.src('dist/**/*').pipe(gitHubPages);
+gulp.task("deploy", ["build"], function () {
+  var gitHubPages = require("gulp-gh-pages")({branch: "master"});
+  return gulp.src("dist/**/*").pipe(gitHubPages);
 });
 
-gulp.task('default', ['build']);
+gulp.task("default", ["build"]);
