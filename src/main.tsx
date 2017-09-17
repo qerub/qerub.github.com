4e6b356da4feddcd8e8c92ed0bc6aa4e72d410c2
Christoffer Sawicki <christoffer.sawicki@gmail.com>
2017-09-17 21:13:32 +0200

    Change casing of the Open Hub name

--- src/main.tsx
+++ src/main.tsx
@@ -15,7 +15,7 @@ function main() {
 
   let body = <div>
     <h1><a href="http://qerub.se/">My Personal Website</a></h1>
-    <h1><a href="https://www.openhub.net/accounts/qerub/positions">My Page on Open HUB</a></h1>
+    <h1><a href="https://www.openhub.net/accounts/qerub/positions">My Page on Open Hub</a></h1>
     <h1><a href="https://github.com/qerub">My GitHub Repositories</a></h1>
     {repoContainer = <p><em>Loading…</em></p>}
     <h1><a href="https://gist.github.com/qerub">My Gists</a></h1>
